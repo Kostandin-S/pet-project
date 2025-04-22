40a334ef6efaef2d321e71137a08bf208cbcd37f
Kostandin-S <kostandin.dervishaj@sigma.software>
2025-04-22 15:21:35 +0200

    Cleaned up tracked files per updated .gitignore

--- api-gateway/dist/constants/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    BASE_PATH: "/api/v1",
-    AUTH: "/auth",
-    USERS: "/users",
-    BOOKS: "/books",
-    HEALTH: "/health",
-};
